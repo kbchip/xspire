1a17daeb353ab7a5c511b1019a75cce6f1a74ff3
kbchip <charlieshaskins@gmail.com>
2025-07-06 18:12:14 -0700

    Modify server.js to use Express instead of http-server

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-const PORT=3000; 
+app.use(express.static(__dirname));
 
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+app.listen('3000');
+console.log('working on 3000');
